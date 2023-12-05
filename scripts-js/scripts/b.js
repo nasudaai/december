6f1da0c7b5759111a2d87659156fe28a30f7e4b7
nasuda <nasuda908@gmail.com>
2023-12-05 21:00:43 +0900

    nigth loop code write

---
+++ scripts-js/scripts/b.js
@@ -0,0 +1,19 @@
+for (let i = 0; i <= 10; i++) {
+  console.log(i)
+}
+
+
+// for -> while
+
+function forLikeWhile() {
+  let i = 0;
+  // cond 
+  let count = i;
+  while(count  <= 10) {
+    console.log(i);
+
+    //step
+
+    const count = i + 1;
+  }
+}
